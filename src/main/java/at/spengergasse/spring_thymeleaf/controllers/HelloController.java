7349bd8a140d4562f7bbee850b2895d5a598725b
jov230379 <JOV230379@spengergasse.at>
2026-02-19 09:25:14 +0100

    irrelevant changes

--- src/main/java/at/spengergasse/spring_thymeleaf/controllers/HelloController.java
+++ src/main/java/at/spengergasse/spring_thymeleaf/controllers/HelloController.java
@@ -12,6 +12,7 @@ public class HelloController {
 
     @GetMapping("")
     public  String index(Model model) {
+        //add date to model
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy");
         model.addAttribute("today", LocalDate.now().format(dtf));
         return "index";
